cfe4650a4cc7a37f4094061060f951bf3bc2f45f
Muhammad Issa <muhammadissa848@gmail.com>
2024-10-20 21:06:13 +0500

    Update AddRecipe

--- src/Pages/AddRecipe.jsx
+++ src/Pages/AddRecipe.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Header from '../components/Header'; 
 import Footer from '../components/Footer';
 import { 
@@ -37,7 +37,7 @@ const AddRecipe = () => {
   const onFinish = (values) => {
     console.log('Form values:', values);
     message.success('Recipe added successfully!');
-  navigate('/')
+     navigate = ('/')
     // Here you would typically send the data to your backend
   };
 
